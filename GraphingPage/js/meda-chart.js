03450d35ae0f61a45ddbf45b521e388210c6f130
Ryan Rickerl <59903232+ryanrickerl@users.noreply.github.com>
2023-03-16 13:19:30 -0600

    Closes #61 (#62)

--- GraphingPage/js/meda-chart.js
+++ GraphingPage/js/meda-chart.js
@@ -1,50 +1,59 @@
+const plotGraph = function () {
+    let parentDataDir = getParentDataDir(sol.value);
+    let solId = sol.value.padStart(4, '0');
+    let rawPSDataURL = "https://sdp.boisestate.edu/pds/data/PDS4/Mars2020/mars2020_meda/data_derived_env/" + parentDataDir + "/sol_" + solId + "/WE__" + solId + "___________DER_PS__________________P01.CSV";
 
-const myChart = document.getElementById('graph');
-const rawPSDataURL = 'https://sdp.boisestate.edu/pds/data/PDS4/Mars2020/mars2020_meda/data_derived_env/sol_0300_0419/sol_0304/WE__0304___________DER_PS__________________P01.CSV';
+    console.log("Sol ID: " + solId);
+    console.log("Parent data dir: " + parentDataDir);
+    console.log(rawPSDataURL);
 
-const medaSol = 304;
-const startBoundary = convertToSeconds('07:23:45');
-const endBoundary = convertToSeconds('07:34:56');
+    // This block is for PRESSURE data
+    d3.csv(rawPSDataURL).then(function(rawData) {
+        var xField = 'LMST';
+        var yField = 'PRESSURE';
 
-d3.csv(rawPSDataURL).then(function(rawData) {
-    var data = prepPSData(rawData);
-    var layout = {
-        showlegend: true,
-        title: "Time series Mars 2020 MEDA data for Sol " + medaSol,
-        xaxis: {
-            automargin: true,
-            tickangle: 45,
-            title: {
-                text: 'LMST',
-                standoff: 16
+        var data = prepData(rawData, xField, yField);
+
+        var layout = {
+            showlegend: true,
+            title: "Mars 2020 MEDA Data - Sol " + sol.value + ", " + startTime.value + " to " + endTime.value,
+            xaxis: {
+                automargin: true,
+                tickangle: 45,
+                title: {
+                    text: xField,
+                    standoff: 16
+                }
+            },
+            yaxis: {
+                autorange: true,
+                rangemode: "normal",
+                title: {
+                    text: yField,
+                    standoff: 16
+                }
             }
-        },
-        yaxis: {
-            autorange: true,
-            rangemode: "normal",
-            title: {
-                text: 'PRESSURE',
-                standoff: 16
-            }
-        }
-    };
+        };
 
-    var config = {
-        displayModeBar: true,
-        displaylogo: false,
-        modeBarButtonsToRemove: ['autoScale2d','lasso2d','select2d']
-    };
+        var config = {
+            displayModeBar: true,
+            displaylogo: false,
+            modeBarButtonsToRemove: ['autoScale2d','lasso2d','select2d']
+        };
 
-    Plotly.newPlot(myChart, data, layout, config);
-});
+        Plotly.newPlot(myChart, data, layout, config);
+    });
+}
 
-function prepPSData(rawData) {
+function prepData(rawData, xField, yField) {
     var x = [];
     var y = [];
-    var xField = 'LMST';
-    var yField = 'PRESSURE';
+
+    var startBoundary = convertToSeconds(startTime.value);
+    var endBoundary = convertToSeconds(endTime.value);
 
     rawData.forEach(function(datum, i) {
+
         let ts = (datum[xField].match(/\d{2}:\d{2}:\d{2}/))[0];
         let seconds = convertToSeconds(ts);
 
@@ -68,3 +77,25 @@ function convertToSeconds(str) {
 
     return (data[0] * 3600) + (data[1] * 60) + data[2];
 }
+
+function getParentDataDir(n) {
+    if (n <= 89) {
+        return 'sol_0000_0089';
+    }
+    else if (n <= 179) {
+        return 'sol_0090_0179';
+    }
+    else if (n <= 299) {
+        return  'sol_0180_0299';
+    }
+    else if (n <= 419) {
+        return 'sol_0300_0419';
+    }
+    else if (n <= 539) {
+        return 'sol_0420_0539';
+    }
+
+    return;
+}
+
+plotGraphBtn.addEventListener('click', plotGraph, false);
