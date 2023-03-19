2bd095d1faaff590f1b154fc47cc9282461dfb4f
ryanrickerl <ryanrickerl@u.boisestate.edu>
2023-03-18 18:22:27 -0600

    Closes #70

--- GraphingPage/js/muldoon.js
+++ GraphingPage/js/muldoon.js
@@ -1,40 +1,33 @@
 //--------------------------------------------------------------
+// Mars Sol length: 24h 39m 35.244s
+// Perseverance (rover) landed 2021-02-18 20:55 UTC
+//--------------------------------------------------------------
+const MILLIS_IN_MARS_SOL = 88775244;
+const MISSION_START_DATE = new Date('2021-02-19T00:00:00');
+const MISSION_START_MILLIS = MISSION_START_DATE.getTime();
+const CURRENT_MISSION_SOL = Math.ceil((Date.now() - MISSION_START_MILLIS) / MILLIS_IN_MARS_SOL);
+
+//--------------------------------------------------------------
 // Muldoon Graphing Page constants for UI input elements.
 //--------------------------------------------------------------
+const uiMsg = document.getElementById('uiMsg');
 const sol = document.getElementById('sol');
 const solLabel = document.getElementById('solLabel');
 const startTime = document.getElementById('startTime');
 const endTime = document.getElementById('endTime');
 const sensor = document.getElementById('sensor');
-const myChart = document.getElementById('graph');
 const plotGraphBtn = document.getElementById('plotGraphBtn');
+const myChart = document.getElementById('graph');
 const medaFileList = [];
 
-//--------------------------------------------------------------
-// Mars Sol length: 24h 39m 35.244s
-// Perseverance (rover) landed 2021-02-18 20:55 UTC
-//--------------------------------------------------------------
-function setMaxSol(sol, solLabel) {
-  const MILLIS_IN_MARS_SOL = 88775244;
-  const MISSION_START_DATE = new Date('2021-02-19T00:00:00');
-  const MISSION_START_MILLIS = MISSION_START_DATE.getTime();
-  const CURRENT_MISSION_SOL = Math.ceil((Date.now() - MISSION_START_MILLIS) / MILLIS_IN_MARS_SOL);
-
-  sol.max = CURRENT_MISSION_SOL;
-  solLabel.innerHTML = "Sol (1-" + CURRENT_MISSION_SOL + "):";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  setMaxSol(sol, solLabel);
-});
+  //--------------------------------------------------------------
+  // Initialize an Object with available MEDA data CSV file
+  //--------------------------------------------------------------
+  let medaDerivedInvURL = "https://sdp.boisestate.edu/pds/data/PDS4/Mars2020/mars2020_meda/data_derived_env/collection_meda_data_derived_env_inventory.csv";
+  let medaCalibratedInvURL = "https://sdp.boisestate.edu/pds/data/PDS4/Mars2020/mars2020_meda/data_calibrated_env/collection_meda_data_calibrated_env_inventory.csv";
 
-//--------------------------------------------------------------
-// Initialize an Object with available MEDA data CSV file
-//--------------------------------------------------------------
-const initMedaFileList = function() {
-  let medaInvURL = "https://sdp.boisestate.edu/pds/data/PDS4/Mars2020/mars2020_meda/data_derived_env/collection_meda_data_derived_env_inventory.csv";
-
-  d3.text(medaInvURL).then(function(data) {
+  d3.text(medaDerivedInvURL).then(function(data) {
     d3.csvParseRows(data, (d, i) => {
       let rowData = d[1].split(/[:]+/);
       let revision = rowData[6].replace(/\.\d$/, '');
@@ -43,19 +36,22 @@ const initMedaFileList = function() {
       let sensorName = ((rowData[5].match(/(ancillary|ps|rhs|tirs|ws)/i))[0]).toUpperCase();
 
       if (fileId !== NaN) {
-        let parentDir = getFileDir(fileId);
+        let parentDir = getParentDir(fileId);
+
+        sol.max = Math.max(fileId, sol.max);
 
         if (parentDir !== undefined) {
           let fileDir = "sol_" + fileName.match(/\d{4}/);
 
           let medaFile = {
             id: fileId,
+            collection: 'data_derived_env',
             parent: parentDir,
             directory: fileDir,
             filename: fileName,
             revision: revision,
             sensor: sensorName,
-            xField: 'LMST',
+            xField: 'LTST',
             yField: setYField(sensorName)
           }
 
@@ -66,10 +62,61 @@ const initMedaFileList = function() {
         }
       }
     });
-  });
-}
 
-function getFileDir(num) {
+    solLabel.innerHTML = "Select Sol (1 - " + sol.max + "):";
+
+    if (sol.max !== CURRENT_MISSION_SOL) {
+      uiMsg.innerHTML = "(note: current mission Sol is " + CURRENT_MISSION_SOL + " w/ MEDA data available up to Sol " + sol.max + ")";
+      uiMsg.hidden = false;
+    }
+  });
+
+  d3.text(medaCalibratedInvURL).then(function(data) {
+    d3.csvParseRows(data, (d, i) => {
+      let rowData = d[1].split(/[:]+/);
+      let revision = rowData[6].replace(/\.\d$/, '');
+      let fileName = (rowData[5] + revision.padStart(2, '0') + ".csv").toUpperCase();
+      let fileId = Number.parseInt(fileName.match(/\d{4}/));
+      let sensorName = ((rowData[5].match(/(ats|eng|ps|rds|rhs|tirs|ws)/i))[0]).toUpperCase();
+
+      if (fileId !== NaN) {
+        let parentDir = getParentDir(fileId);
+
+        sol.max = Math.max(fileId, sol.max);
+
+        if (parentDir !== undefined) {
+          let fileDir = "sol_" + fileName.match(/\d{4}/);
+
+          let medaFile = {
+            id: fileId,
+            collection: 'data_calibrated_env',
+            parent: parentDir,
+            directory: fileDir,
+            filename: fileName,
+            revision: revision,
+            sensor: sensorName,
+            xField: 'LTST',
+            yField: setYField(sensorName)
+          }
+
+          medaFileList.push(medaFile);
+        }
+        else {
+          console.log("Failed to get location for fileId: " + fileId + " filename: " + filename + " revision: " + revision + " sensor: " + sensorName);
+        }
+      }
+    });
+
+    solLabel.innerHTML = "Select Sol (1 - " + sol.max + "):";
+
+    if (sol.max !== CURRENT_MISSION_SOL) {
+      uiMsg.innerHTML = "(note: current mission Sol is " + CURRENT_MISSION_SOL + " w/ MEDA data available up to Sol " + sol.max + ")";
+      uiMsg.hidden = false;
+    }
+  });
+});
+
+function getParentDir(num) {
   if (num <= 89) {
     return 'sol_0000_0089';
   }
@@ -92,14 +139,12 @@ function getFileDir(num) {
 function setYField(str) {
   switch (str) {
     case "PS":
-      return "PRESSURE";
+      return ["PRESSURE"];
     case "WS":
-      return "HORIZONTAL_WIND_SPEED";
-    case "RHS":
-      return "LOCAL_RELATIVE_HUMIDITY";
+      return ["HORIZONTAL_WIND_SPEED", "WIND_SPEED_DIRECTION"];
+    case "ATS":
+      return ["ATS_LOCAL_TEMP1", "ATS_LOCAL_TEMP2", "ATS_LOCAL_TEMP3", "ATS_LOCAL_TEMP4", "ATS_LOCAL_TEMP5"];
     default:
       return;
   }
 }
-
-initMedaFileList();
