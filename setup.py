fd967ab22bb49dfe138a5bb795e96a0fbd322e38
Matt R <85653509+4runnerlife@users.noreply.github.com>
2023-04-02 14:52:51 -0600

    Update setup.py
    Signed-off-by: Matt R <85653509+4runnerlife@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy','pds4-tools','emcee', 'matplotlib', 'scipy', 'astropy', 'pandas', 'statsmodels', 'selenium', 'webdrivermanager'],  # Optional
+    install_requires=['numpy','pds4-tools','emcee', 'matplotlib', 'scipy', 'astropy', 'pandas', 'statsmodels', 'selenium', 'webdriver-manager'],  # Optional
 
 
     # List additional groups of dependencies here (e.g. development
