c6f9f81f4a31e1c3d4db4f48df805db37a20e351
Ryan Rickerl <59903232+ryanrickerl@users.noreply.github.com>
2023-04-10 17:55:15 -0600

    Closes #90 (#93)
    Migrated URL parsing and data export functionality.

--- muldoon-webapp/assets/js/meda-chart.js
+++ muldoon-webapp/assets/js/meda-chart.js
@@ -1,6 +1,11 @@
+let plotChartFromURL = (window.location.search !== "");
+
 const generatePlot = function () {
   let sensorName = sensor.options[sensor.selectedIndex].text;
   let medaRef = medaFileList.find(ds => ds.id === Number.parseInt(sol.value) && ds.sensor === sensor.value);
+
+  shareURLBtn.disabled = true;
+  exportDataBtn.disabled = true;
 
   if (medaRef === undefined) {
     let title = "Data not found";
@@ -10,7 +15,7 @@ const generatePlot = function () {
 
     return;
   }
-  
+
   let collection = medaRef.collection;
   let parent = medaRef.parent;
   let directory = medaRef.directory;
@@ -24,7 +29,7 @@ const generatePlot = function () {
     var xField = medaRef.xField;
     var yField = medaRef.yField[0];
 
-    var plotTitle = sensorName + " for Sol " + sol.value + ", " + startTime.value + " to " + endTime.value;
+    var plotTitle = sensorName + "<br><sup>Sol " + sol.value + " - " + startTime.value + " to " + endTime.value + "</sup>";
     var data = prepData(rawData, xField, yField);
 
     var layout = {
@@ -34,6 +39,7 @@ const generatePlot = function () {
           size: 14
         }
       },
+      height: 750,
       width: 1400,
       //showlegend: true,
       title: {
@@ -71,15 +77,36 @@ const generatePlot = function () {
       }
     };
 
+    let savePNGFilename = "sol_" + sol.value + "_" + (Date.now()/1000);
+
+    if (plotChartFromURL) {
+      let params = new URLSearchParams(window.location.search);
+      layout.xaxis.range = [params.get('x1'), params.get('x2')];
+      layout.yaxis.range = [params.get('y1'), params.get('y2')];
+
+      plotChartFromURL = false;
+    }
+
     var config = {
       displayModeBar: true,
       displaylogo: false,
-      //downloadImageFilename: ???,
       modeBarButtonsToRemove: ['autoScale2d', 'lasso2d', 'select2d'],
-      responsive: true
+      responsive: true,
+      toImageButtonOptions: {
+        format: 'png',
+        filename: savePNGFilename,
+        scale: 1
+      }
     };
 
     myPlot.innerHTML = "";
+
+    exportDataBtn.onclick = function() {
+      window.open(rawDataURL);
+    }
+
+    shareURLBtn.disabled = false;
+    exportDataBtn.disabled = false;
 
     Plotly.newPlot(myPlot, data, layout, config);
   });
@@ -128,4 +155,26 @@ function displayModal(t, m) {
     myModal.toggle();
 }
 
+function shareURL() {
+  if (myPlot.classList.contains('js-plotly-plot')) {
+    let urlBuilder = new URLSearchParams();
+
+    urlBuilder.set('sol', sol.value);
+    urlBuilder.set('start', startTime.value);
+    urlBuilder.set('end', endTime.value);
+    urlBuilder.set('sensor', sensor.selectedIndex);
+    urlBuilder.set('x1', myPlot.layout.xaxis.range[0]);
+    urlBuilder.set('x2', myPlot.layout.xaxis.range[1]);
+    urlBuilder.set('y1', myPlot.layout.xaxis.range[0]);
+    urlBuilder.set('y2', myPlot.layout.xaxis.range[1]);
+
+    let url = window.location.origin + window.location.pathname + '?' + urlBuilder.toString();
+
+    navigator.clipboard.writeText(url);
+
+    alert("URL copied to clipboard");
+  }
+}
+
 generatePlotBtn.addEventListener('click', generatePlot, false);
+shareURLBtn.addEventListener('click', shareURL, false);
