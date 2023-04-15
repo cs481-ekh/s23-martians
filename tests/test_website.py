5a75b58ac6f0cc46c045a9f7486b7c649775c3b2
Matt R <85653509+4runnerlife@users.noreply.github.com>
2023-04-15 16:42:55 -0600

    Closes #92 (#100)
    * First test for edge
    
    * Implemented the test for Edge
    
    ---------
    
    Co-authored-by: 4runnerlife <matt.rock45@gmail.com>

--- tests/test_website.py
+++ tests/test_website.py
@@ -2,8 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.firefox.options import Options
-
-
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 
 # Testing for Chrome Browser
@@ -213,6 +212,116 @@ def test_linkToAstrojackFirefox():
    try:
       options = Options()
       driver = webdriver.Firefox(options=options)
+
+      driver.get("https://sdp.boisestate.edu/s23-martians/")
+    
+      driver.implicitly_wait(1)
+      try:
+         if(driver.find_element_by_link_text("Astrojack")):
+            assert True
+      except: 
+            assert False
+
+      driver.quit()
+   except:
+      print("Error")
+
+# Testing for Edge Browser
+
+def test_titleEdge():
+   try:
+      service = Service(verbose = True)
+      driver = webdriver.Edge(service=service)
+
+      driver.get("https://sdp.boisestate.edu/s23-martians/")
+    
+      driver.implicitly_wait(1)
+      try:
+         if(driver.find_element_by(by=By.Name, value = "titleBar")):
+            assert True
+      except: 
+            assert False
+
+      driver.quit()
+   except:
+      print("Error")
+
+def test_textBoxEdge():
+   try:
+      service = Service(verbose = True)
+      driver = webdriver.Edge(service=service)
+
+      driver.get("https://sdp.boisestate.edu/s23-martians/")
+    
+      driver.implicitly_wait(1)
+      try:
+         if(driver.find_element(by=By.NAME, value="startTime")):
+            assert True
+      except: 
+            assert False
+
+      driver.quit()
+   except:
+      print("Error")
+
+def test_plotButtonEdge():
+   try:
+      service = Service(verbose = True)
+      driver = webdriver.Edge(service=service)
+
+      driver.get("https://sdp.boisestate.edu/s23-martians/")
+    
+      driver.implicitly_wait(1)
+      try:
+         if(driver.find_element(by=By.NAME, value="generatePlotBtn")):
+            assert True
+      except: 
+            assert False
+
+      driver.quit()
+   except:
+      print("Error")
+
+def test_sensorDropDownEdge():
+   try:
+      service = Service(verbose = True)
+      driver = webdriver.Edge(service=service)
+
+      driver.get("https://sdp.boisestate.edu/s23-martians/")
+    
+      driver.implicitly_wait(1)
+      try:
+         if(driver.find_element(by=By.NAME, value="sensor")):
+            assert True
+      except: 
+            assert False
+
+      driver.quit()
+   except:
+      print("Error")
+
+def test_linkToMEDAEdge():
+   try:
+      service = Service(verbose = True)
+      driver = webdriver.Edge(service=service)
+
+      driver.get("https://sdp.boisestate.edu/s23-martians/")
+    
+      driver.implicitly_wait(1)
+      try:
+         if(driver.find_element_by_link_text("Mars 2020 PDS")):
+            assert True
+      except: 
+            assert False
+
+      driver.quit()
+   except:
+      print("Error")
+
+def test_linkToAstrojackEdge():
+   try:
+      service = Service(verbose = True)
+      driver = webdriver.Edge(service=service)
 
       driver.get("https://sdp.boisestate.edu/s23-martians/")
     
